c5c50165e6a5bb1dc97188dc933e6b54377694f5
cyh5kf <1269415602@qq.com>
2017-10-15 23:18:09 +0800

    fix router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'product', component: ProductComponent}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'product/:id', component: ProductComponent},
+  {path: '**', component: Code404Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { HomeComponent } from './home/home.component';
+import { Code404Component } from './code404/code404.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, ProductComponent
+    AppComponent, HomeComponent, ProductComponent, Code404Component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [],
